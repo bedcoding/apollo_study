57f931805ed70af1e83be3b4f46aea2cb42f16b5
wordword <ggavi2000@naver.com>
2020-09-21 19:24:22 +0900

    클라이언트

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ styled-components : CSS를 위한 것
 react-router-dom : router를 위한 것
 apollo-boost, @apollo/react-hooks : hooks를 사용하기 위한 것.
 graphql : GraphQL을 위한 것.
+
+----------
+
+GraphQl Yoga가 바로 쓸 수 있게 준비를 다 해둔 server고
+반대로 apollo-boost가 client 쪽이다.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./apollo"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// React Application을 client로 감싸야 한다. ApolloProvider는 client가 필요하다.
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
